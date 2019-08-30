6fe77c6f9f4d87099df21be3ece6e781f17efb94
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-08-30 18:49:40 -0400

    duped config

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -11,25 +11,11 @@ data_path="/etc"
 email="vishnu-srivastava@hotmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
 
-
-if [ ! -e "$data_path/letsencrypt/options-ssl-nginx.conf" ] || [ ! -e "$data_path/letsencrypt/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/letsencrypt"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/tls_configs/options-ssl-nginx.conf > "$data_path/letsencrypt/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/letsencrypt/ssl-dhparams.pem"
-  echo
-fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
