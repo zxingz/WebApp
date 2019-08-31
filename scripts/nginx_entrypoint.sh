29b510210dd218636fe1a9508b9dc3dd45ef6942
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-08-30 20:46:32 -0400

    get certbot to work

--- scripts/nginx_entrypoint.sh
+++ scripts/nginx_entrypoint.sh
@@ -3,4 +3,6 @@
 /etc/init.d/nginx start
 sleep 2
 certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com -d the-software.dev -n
+/etc/init.d/nginx stop
+mv /etc/nginx/conf.d/default.update /etc/nginx/conf.d/default.conf
 tail -f /dev/null
