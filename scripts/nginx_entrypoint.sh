b56b7adcaeeb04128dd59ad3d27053a8991c50e6
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-08-30 20:03:45 -0400

    fix format

--- scripts/nginx_entrypoint.sh
+++ scripts/nginx_entrypoint.sh
@@ -2,3 +2,5 @@
 
 mkdir -p /etc/letsencrypt/live/the-software.dev
 openssl req -x509 -nodes -newkey rsa:1024 -days 1 -keyout '/etc/letsencrypt/live/the-software.dev/privkey.pem' -out '/etc/letsencrypt/live/the-software.dev/fullchain.pem' -subj '/CN=localhost'
+nginx -g 'daemon off;'
+tail -f /dev/null
