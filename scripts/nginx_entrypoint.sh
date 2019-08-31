f8c1793f0005f29cd72c1f1e59b57c5b3d9d0906
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-08-30 20:29:51 -0400

    test new strat

--- scripts/nginx_entrypoint.sh
+++ scripts/nginx_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p /etc/letsencrypt/live/the-software.dev
-openssl req -x509 -nodes -newkey rsa:1024 -days 1 -keyout '/etc/letsencrypt/live/the-software.dev/privkey.pem' -out '/etc/letsencrypt/live/the-software.dev/fullchain.pem' -subj '/CN=localhost'
-nginx -g 'daemon off;'
+/etc/init.d/nginx start
+sleep 2
+certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com -d the-software.dev -n
 tail -f /dev/null
