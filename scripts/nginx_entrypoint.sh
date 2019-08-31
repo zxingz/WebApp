e97b5eb874e56b435f6e8e7308dc862a02838f54
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-08-30 21:00:18 -0400

    test best practice

--- scripts/nginx_entrypoint.sh
+++ scripts/nginx_entrypoint.sh
@@ -5,4 +5,4 @@ sleep 2
 certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com -d the-software.dev -n
 /etc/init.d/nginx stop
 mv /etc/nginx/conf.d/default.update /etc/nginx/conf.d/default.conf
-tail -f /dev/null
+nginx -g 'daemon off;'
