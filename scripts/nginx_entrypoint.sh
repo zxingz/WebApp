80808567ddedc7ea48a6f34a229b033ceeb1b9fa
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-08-30 22:48:34 -0400

    remove wildcard

--- scripts/nginx_entrypoint.sh
+++ scripts/nginx_entrypoint.sh
@@ -2,7 +2,7 @@
 
 /etc/init.d/nginx start
 sleep 2
-certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com  -d 'the-software.dev,*.the-software.dev' -n
+certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com  -d 'the-software.dev,www.the-software.dev' -n
 /etc/init.d/nginx stop
 yes | cp -rf /etc/nginx/conf.d/default.update /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
