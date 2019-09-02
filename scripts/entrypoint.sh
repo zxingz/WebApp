4de436dca1d7264108ca3a1db430aebfcadf7ddb
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-09-02 15:55:09 -0400

    unifying app

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,4 +5,5 @@ sleep 2
 certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com  -d 'the-software.dev,www.the-software.dev,vishnu.the-software.dev' -n
 /etc/init.d/nginx stop
 yes | cp -rf /etc/nginx/conf.d/default.update /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+/etc/init.d/nginx start
+python3 app.py
