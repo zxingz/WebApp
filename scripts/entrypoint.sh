5f7a987622dbd9754021453e5ca3dabf9fb6d644
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-09-03 14:53:21 -0400

    server page

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo /etc/init.d/nginx start
+#sudo /etc/init.d/nginx start
 sleep 2
 #sudo certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com  -d 'the-software.dev,www.the-software.dev' -n
 sudo etc/init.d/nginx stop
-#sudo yes | cp -rf /etc/nginx/conf.d_new/default.conf /etc/nginx/conf.d/default.conf
-#sudo /etc/init.d/nginx start
+sudo yes | cp -rf /etc/nginx/conf.d_new/default.conf /etc/nginx/conf.d/default.conf
+sudo /etc/init.d/nginx start
 python3 app.py
