8973e0af2f3444a8bbe1496e54898a7288f3b9fb
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-09-03 13:14:14 -0400

    change conf

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,8 +2,8 @@
 
 /etc/init.d/nginx start
 sleep 2
-certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com  -d 'the-software.dev,www.the-software.dev,vishnu.the-software.dev' -n
+certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com  -d 'the-software.dev,www.the-software.dev' -n
 /etc/init.d/nginx stop
-yes | cp -rf /etc/nginx/conf.d/default.update /etc/nginx/conf.d/default.conf
+yes | cp -rf /etc/nginx/conf.d_new/default.conf/etc/nginx/conf.d/default.conf
 /etc/init.d/nginx start
 python3 app.py
