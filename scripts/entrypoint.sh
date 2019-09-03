9c9acbbf7c84076f87342658688b921ab8c19c73
Vishnu Srivastava <vishnu-srivastava@hotmail.com>
2019-09-03 13:38:27 -0400

    fix

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,6 @@ sudo /etc/init.d/nginx start
 sleep 2
 sudo certbot --nginx --agree-tos --email vishnu.srivastava@hotmail.com  -d 'the-software.dev,www.the-software.dev' -n
 /sudo etc/init.d/nginx stop
-sudo yes | cp -rf /etc/nginx/conf.d_new/default.conf/etc/nginx/conf.d/default.conf
+sudo yes | cp -rf /etc/nginx/conf.d_new/default.conf /etc/nginx/conf.d/default.conf
 sudo /etc/init.d/nginx start
 python3 app.py
